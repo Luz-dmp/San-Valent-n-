8e555492bb6faadbf70fc35de49785426adfa8eb
Luz-dmp <luzmendezpenate@gmail.com>
2025-02-04 19:49:16 -0500

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById("yes").addEventListener("click", function() {
+    alert("¡Sabía que dirías que sí! ❤️ Te amo mucho 🥰");
+});
+
+document.getElementById("no").addEventListener("click", function() {
+    alert("Oh no... 😢 Pero Spiderman no se rinde, ¡lo intentaré de nuevo!");
+});
